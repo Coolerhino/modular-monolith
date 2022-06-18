66f38b2f72c3687c9d02293a9d57e0c4a92542e6
Coolerhino <emilian3310@gmail.com>
2022-06-18 14:16:48 +0200

    Add host service and controller actions

--- src/Bootstraper/Confab.Bootstrapper/Program.cs
+++ src/Bootstraper/Confab.Bootstrapper/Program.cs
@@ -7,8 +7,8 @@ namespace Confab.Bootstraper
     public class Program
     {
         public static Task Main(string[] args)
-        =>
-            CreateHostBuilder(args).Build().RunAsync();
+            =>
+                CreateHostBuilder(args).Build().RunAsync();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
---
+++ src/Modules/Conferences/Confab.Modules.Conferences.Core/Policies/ConferenceDeletionPolicy.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Confab.Modules.Conferences.Core.Entities;
+using Confab.Shared.Abstractions;
+
+namespace Confab.Modules.Conferences.Core.Policies
+{
+    internal class ConferenceDeletionPolicy : IConferenceDeletionPolicy
+    {
+        private readonly IClock _clock;
+
+        public ConferenceDeletionPolicy(IClock clock)
+        {
+            _clock = clock;
+        }
+        public async Task<bool> CanDeleteAsync(Conference conference)
+        {
+            //todo check participants if any
+            var canDelete = _clock.CurrentDate().Date.AddDays(7) < conference.From.Date;
+
+            return canDelete;
+        }
+    }
+}
